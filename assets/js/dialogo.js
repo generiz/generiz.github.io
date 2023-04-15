30d9bebd2daf7797b9bbb6296fc364a29492374c
generiz <39973192+generiz@users.noreply.github.com>
2023-04-15 00:58:39 -0600

    script mensaje centrar proyecto

--- assets/js/dialogo.js
+++ assets/js/dialogo.js
@@ -1,13 +1,17 @@
 function showEasterEggHint() {
-    const hint = document.getElementById("easter-egg-hint");
-    hint.style.display = "block";
+  const hint = document.getElementById("easter-egg-hint");
+  hint.style.display = "block";
 }
 
+function isMobileDevice() {
+  return window.innerWidth <= 767;
+}
 
 document.addEventListener("DOMContentLoaded", function() {
+  if (!isMobileDevice()) {
     setTimeout(showEasterEggHint, 10000);
-    document.getElementById("close-hint-btn").addEventListener("click", function () {
-      document.getElementById("easter-egg-hint").style.display = "none";
-    });
+  }
+  document.getElementById("close-hint-btn").addEventListener("click", function() {
+    document.getElementById("easter-egg-hint").style.display = "none";
   });
-  
+});
