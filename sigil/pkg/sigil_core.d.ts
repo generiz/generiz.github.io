39a5c02cdad98c3bb3b0d51f28bfa089373bf066
generiz <39973192+generiz@users.noreply.github.com>
2026-08-30 14:17:12 +0000

    serve sigil demo first-party

---
+++ sigil/pkg/sigil_core.d.ts
@@ -0,0 +1,49 @@
+/* tslint:disable */
+/* eslint-disable */
+export function sigil_demo_version(): string;
+export function run_protocol_demo(input: string, requested_loss: number): string;
+export class DemoSession {
+  free(): void;
+  constructor(input: string);
+  run(missing_slots_csv: string): string;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_demosession_free: (a: number, b: number) => void;
+  readonly demosession_new: (a: number, b: number) => [number, number, number];
+  readonly demosession_run: (a: number, b: number, c: number) => [number, number, number, number];
+  readonly run_protocol_demo: (a: number, b: number, c: number) => [number, number, number, number];
+  readonly sigil_demo_version: () => [number, number];
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __externref_table_alloc: () => number;
+  readonly __wbindgen_export_2: WebAssembly.Table;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __externref_table_dealloc: (a: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
